5282df449451eb842601caf0e6960387881d56c2
SalasDelil <salasdelil8@gmail.com>
2023-05-18 23:36:15 +0300

    app modified

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 //@ts-nocheck
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import Layout from "../components/Layout";
 
